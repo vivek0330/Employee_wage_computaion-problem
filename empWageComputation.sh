607af1e7ea2261b80fa306de7b55a727a5cdd24a
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-13 19:36:24 +0530

    Display employee present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,14 @@
 #!/bin/bash -x
+#welcome comment for employee Wage computation problem"
 echo "Welcome to Employee Wage Computation Problem"
+
+#initialize veriables
+isPresent=1
+
+#check condition and show employee present or not
+if [ $((RANDOM%2)) -eq $isPresent ]
+then 
+	echo "Employee is present"
+else
+	echo "Employee is Absent"
+fi
