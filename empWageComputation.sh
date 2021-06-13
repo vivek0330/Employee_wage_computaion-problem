973748eacbe603296f0601440bda072fb8d3d278
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-13 22:10:13 +0530

    calculate daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,11 +4,19 @@ echo "Welcome to Employee Wage Computation Problem"
 
 #initialize veriables
 isPresent=1
+wage_per_hour=20
 
 #check condition and show employee present or not
 if [ $((RANDOM%2)) -eq $isPresent ]
 then 
 	echo "Employee is present"
+	hours_perday=8
 else
 	echo "Employee is Absent"
+	hours_perday=0
 fi
+
+#calculate daily employee wage
+echo "Daily employee wage per hours is 20"
+Daily_employee_wage=$(($wage_per_hour*$hours_perday))
+echo "$Daily_employee_wage rs. per day"
