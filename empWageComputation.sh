a0486d40ec07ee86e41cf70404391efd7061040b
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 14:20:29 +0530

    Calculate wages for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,29 +1,38 @@
-#!/bin/bash -x
-#welcome comment for employee Wage computation problem"
-echo "Welcome to Employee Wage Computation Problem"
+#!/bin/bash -x 
 
-#initialize veriables
+echo "Welcome to Employee Wage Computation"
+
+#INITIALIZE THE VARIABLES AND CONSTANTS
 WAGE_PER_HOUR=20
-isPart_Time=1
-isFull_Time=2
-checkEmp=$((RANDOM%3))
+IS_PART_TIME=1
+IS_FULL_TIME=2
+WORKING_DAYS_PER_MONTH=20
+salary=0
 
-#CHECK THE CONDITIONS - IF AN EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE
-case $checkEmp in
-	$isFull_Time)
-		echo "This is a Full time Employee."
-		hoursPerDay=8
-		;;
-	$isPart_Time)
-		echo "This is a Part time Employee."
-		hoursPerDay=4
-		;;
-	*)
-		echo "This is neither a Full time nor a Part time Employee."
-		hoursPerDay=0
-		;;
-esac
+#CHECK THE CONDITION - EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE 
+for((days=1; days<=20; days++))
+do
 
-#CALCULATE AND DISPLAY DAILY EMPLOYEE WAGE
-dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay))
-echo "$dailyEmployeeWage rs. per day"
+	#CHECK EMPLOYEE WORKS FOR FULL TIME OR PART TIME IN A DAY
+	checkEmp=$(( RANDOM%3 ))
+	
+	case $checkEmp in	
+		$IS_FULL_TIME)
+			hoursPerDay=8
+			;;
+		$IS_PART_TIME)
+			hoursPerDay=4
+			;;
+		*)
+			hoursPerDay=0
+			;;
+	esac
+
+	#CALCULATE EMPLOYEE WAGES FOR A MONTH
+	dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay)) 
+	salary=$(( $salary+$dailyEmployeeWage ))
+done
+
+#DISPLAY EMPLOYEE'S WAGES FOR A MONTH
+echo "Employee's wages for a month :"
+echo "$salary rs. per month"
