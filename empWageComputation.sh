bcb86d68dd04ccc60ed1440a2ed20d3d755faf35
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 14:46:57 +0530

    Store daily wage along with the total wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -x 
 
-echo "Welcome to Employee Wage Computation."
+echo "Welcome to Employee Wage Computation"
 
 #INITIALIZE THE VARIABLES AND CONSTANTS
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
 WORKING_DAYS_PER_MONTH=20
-days=0
 WORKING_HOURS_LIMIT=100
+days=0
 workingHours=0
+MAX_HOURS_IN_MONTH=10
 
 #CHECK WORKING HOUR PER DAY OF AN EMPLOYEE
 function workingHoursPerDay()
@@ -28,17 +29,25 @@ function workingHoursPerDay()
 	echo $hoursPerDay
 }
 
+#CALCULATE DAILY WAGE
+function calculateDailyWage()
+{
+	localWorkHrs=$1
+	wage=$(($localWorkHrs*$WAGE_PER_HOUR))
+	echo $wage
+}
+
+
 #CALCULATING TOTAL WORKING HOURS
-while [[ $workingHours<$WORKING_HOURS_LIMIT && $days<$WORKING_DAYS_PER_MONTH ]]
+while [[ $workingHours -lt $WORKING_HOURS_LIMIT && $days -lt $WORKING_DAYS_PER_MONTH ]]
 do
 	(( days++ ))
 	hoursPerDay=$( workingHoursPerDay $(( RANDOM%3 )) )
-	workingHours=$(($workingHours+$hoursPerDay))
+	workingHours=$(( $workingHours + $hoursPerDay ))
+	employeeDailyWage[$days]=$( calculateDailyWage $hoursPerDay )
 done
 
-#DISPLAY TOTAL WORKING HOURS
-echo "Total working hours: "
-echo "$workingHours hours. "
-
 #CALCULATE THE TOTAL SALARY 
-salary=$(($workingHours*$WAGE_PER_HOUR))
+salary=$( calculateDailyWage $workingHours )
+
+echo "Daily wage" ${employeeDailyWage[@]}
