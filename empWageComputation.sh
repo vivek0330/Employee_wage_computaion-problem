2e5f2fa376314aa935a54e918acaf6e2d1155bf3
Vivek varshney <Vivek.iitd93@gmail.com>
2021-06-14 14:34:56 +0530

    Calculate wages till a condition 100 hours and 20 days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,21 +1,26 @@
 #!/bin/bash -x 
 
-echo "Welcome to Employee Wage Computation"
+echo "Welcome to Employee Wage Computation."
 
 #INITIALIZE THE VARIABLES AND CONSTANTS
 WAGE_PER_HOUR=20
 IS_PART_TIME=1
 IS_FULL_TIME=2
 WORKING_DAYS_PER_MONTH=20
-salary=0
+days=0
+WORKING_HOURS_LIMIT=100
+workingHours=0
 
-#CHECK THE CONDITION - EMPLOYEE IS A PART TIME OR FULL TIME EMPLOYEE 
-for((days=1; days<=20; days++))
+#CHECK THE CONDITION AND CALCULATE EMPLOYEES'S WORKING HOURS  
+while(( $days<$WORKING_DAYS_PER_MONTH && $workingHours<$WORKING_HOURS_LIMIT ))
 do
+ 	#INCREMENT OF DAYS
+	(( days++ ))
 
-	#CHECK EMPLOYEE WORKS FOR FULL TIME OR PART TIME IN A DAY
+	#RANDOM CHECK
 	checkEmp=$(( RANDOM%3 ))
 	
+	#CHECK THE CONDITION - EMPLOYEE IS A PART TIME OR FULL TIME 
 	case $checkEmp in	
 		$IS_FULL_TIME)
 			hoursPerDay=8
@@ -27,12 +32,14 @@ do
 			hoursPerDay=0
 			;;
 	esac
-
-	#CALCULATE EMPLOYEE WAGES FOR A MONTH
-	dailyEmployeeWage=$(($WAGE_PER_HOUR*$hoursPerDay)) 
-	salary=$(( $salary+$dailyEmployeeWage ))
+	
+	#CALCULATING TOTAL WORKING HOURS OF EMPLOYEE IN A MONTH
+	workingHours=$(( $hoursPerDay + $workingHours ))
 done
 
-#DISPLAY EMPLOYEE'S WAGES FOR A MONTH
-echo "Employee's wages for a month :"
+#CALCULATE EMPLOYEE WAGES FOR A MONTH
+salary=$(( $WAGE_PER_HOUR * $workingHours )) 
+	
+#DISPLAY TOTAL WAGES RAECHED AT THE END OF THE MONTH
+echo "Total salary of an employee at the end of the month :"
 echo "$salary rs. per month"
